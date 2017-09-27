aca9416f4723fbeda241426428c44cf0fbad3167
cono_144425 <rodriguezcono7@gmail.com>
2017-09-27 18:08:12 -0300

    [NO COMPILA] CrearPersonaVaciaTest

--- Obligatorio/Pruebas/PersonaTest.cs
+++ Obligatorio/Pruebas/PersonaTest.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Pruebas
 {
     [TestClass]
-    public class UnitTest1
+    public class PersonaTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CrearPersonaVaciaTest()
         {
+            Persona persona = new Persona();
         }
     }
 }
