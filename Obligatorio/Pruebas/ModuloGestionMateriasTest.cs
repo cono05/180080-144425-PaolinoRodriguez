c92216b629b0168816685645506a48cbcc311ffd
cono_144425 <rodriguezcono7@gmail.com>
2017-10-07 15:03:08 -0300

    [NO COMPILA] AgregarMateriaTest()

---
+++ Obligatorio/Pruebas/ModuloGestionMateriasTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dominio;
+using Persistencia;
+namespace Pruebas
+{
+    [TestClass]
+    public class ModuloGestionMateriasTest
+    {
+        [TestMethod]
+        public void AgregarMateriaTest()
+        {
+            Materia materia = Materia.CrearMateria();
+            materia.Nombre = "Diseño";
+            ModuloGestionMaterias modulo = new ModuloGestionMaterias(IRepositorio repositorio);
+        }
+    }
+}
