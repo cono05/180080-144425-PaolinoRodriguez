31e25f8c526c64b6d3fe440f1b49b120d76741bc
cono_144425 <rodriguezcono7@gmail.com>
2017-10-07 12:41:14 -0300

    Error valores no validos en ruta de acceso

--- Obligatorio/Dominio/Docente.cs
+++ Obligatorio/Dominio/Docente.cs
@@ -10,12 +10,12 @@ namespace Dominio
             this.MateriasQueDicta = new List<Materia>();
         }
 
-        //public static Docente CrearDocente()
-        //{
-        //    Docente docente = new Docente();
-        //    return docente;
-        //}
+        public static Docente CrearDocente()
+        {
+            Docente docente = new Docente();
+            return docente;
+        }
 
-       
+
     }
 }
