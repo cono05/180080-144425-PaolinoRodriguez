e3307f3fe0fc8dd25117662b8ba753829b567397
cono_144425 <rodriguezcono7@gmail.com>
2017-10-07 12:59:50 -0300

    Se crea una primera version de la interfaz(contrato) IRepositorio

---
+++ Obligatorio/Persistencia/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+namespace Persistencia
+{
+    public interface IRepositorio
+    {
+        void AgregarMateria(Materia materia);
+        void EliminarMateria(Materia materia);
+        void AgregarAlumno(Alumno alumno);
+        void EliminarAlumno(Alumno alumno);
+        void AgregarDocente(Docente docente);
+        void EliminarDocente(Docente docente);
+        
+    }
+}
