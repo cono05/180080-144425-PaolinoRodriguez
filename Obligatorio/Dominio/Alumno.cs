d92af3a477d85f11aaca97f219c6d9bbbc7cfa2f
cono_144425 <rodriguezcono7@gmail.com>
2017-10-07 12:35:01 -0300

    no message

--- Obligatorio/Dominio/Alumno.cs
+++ Obligatorio/Dominio/Alumno.cs
@@ -1,7 +1,22 @@
-﻿namespace Dominio
+﻿using System.Collections.Generic;
+
+namespace Dominio
 {
     public class Alumno : Persona
     {
-        
+        public int NumeroDeEstudiante { get; set; }
+        public string Cedula { get; set; }
+        public ICollection<Materia> MateriasInscripto { get; set; }
+
+        public Alumno()
+        {
+            this.Cedula = string.Empty;
+            this.MateriasInscripto = new List<Materia>();
+        }
+        public static Alumno CrearAlumno()
+        {
+            Alumno alumno = new Alumno();
+            return alumno;
+        }
     }
 }
--- Obligatorio/Dominio/Docente.cs
+++ Obligatorio/Dominio/Docente.cs
@@ -2,13 +2,19 @@
 
 namespace Dominio
 {
-    public class Docente: Persona
+    public class Docente : Persona
     {
         public ICollection<Materia> MateriasQueDicta { get; set; }
         public Docente()
         {
             this.MateriasQueDicta = new List<Materia>();
         }
+
+        //public static Docente CrearDocente()
+        //{
+        //    Docente docente = new Docente();
+        //    return docente;
+        //}
 
        
     }
