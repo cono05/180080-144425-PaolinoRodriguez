97be892463a2c4400dd5b1cd703918dae7c39eb3
cono_144425 <rodriguezcono7@gmail.com>
2017-10-09 15:40:54 -0300

    [NO COMPILA] ExisteMateriaConMismoCodigoTrueTest()

--- Obligatorio/Dominio/Materia.cs
+++ Obligatorio/Dominio/Materia.cs
@@ -7,11 +7,13 @@ namespace Dominio
         public ICollection<Alumno> Alumnos { get; set; }
         public ICollection<Docente> Docentes { get; set; }
         public string Nombre { get; set; }
+        public string Codigo { get; set; }
         
 
         public Materia()
         {
             this.Nombre = string.Empty;
+            this.Codigo = string.Empty;
             this.Alumnos = new List<Alumno>();
             this.Docentes = new List<Docente>();
         }
