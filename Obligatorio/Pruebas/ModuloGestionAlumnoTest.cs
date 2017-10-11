3ced8091b7493792fd06e11383ff62ac81609790
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-10-11 18:11:10 -0300

    [NO COMPILA] AgregarAlumnoTest()

---
+++ Obligatorio/Pruebas/ModuloGestionAlumnoTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dominio;
+using Persistencia;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class ModuloGestionAlumnoTest
+    {
+        [TestMethod]
+        public void AgregarAlumnoTest()
+        {
+            Alumno alumno = UtilidadesPruebas.CrearAlumnoDePrueba("Nombre1", "Apellido1", "0000000-1", "nombre1apellido1@gmail.com", 1);
+            RepositorioRam repositorio = new RepositorioRam();
+            ModuloGestionAlumno modulo = new ModuloGestionAlumno(repositorio);
+            modulo.Alta(alumno);
+            Assert.IsTrue(repositorio.Alumnos.Count == 1);
+        }
+    }
+}
