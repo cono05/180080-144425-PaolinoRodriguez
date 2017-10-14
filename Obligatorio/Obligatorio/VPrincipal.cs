db8ea5211d5e89653af8baa4f91cbb152b7f3e4b
cono_144425 <rodriguezcono7@gmail.com>
2017-10-14 20:14:13 -0300

    Se agrega panel en VPrincipal

--- Obligatorio/Obligatorio/VPrincipal.cs
+++ Obligatorio/Obligatorio/VPrincipal.cs
@@ -4,5 +4,28 @@ namespace Obligatorio
 {
     class VPrincipal : Form
     {
+        private Panel panel1;
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(875, 406);
+            this.panel1.TabIndex = 0;
+            // 
+            // VPrincipal
+            // 
+            this.ClientSize = new System.Drawing.Size(875, 406);
+            this.Controls.Add(this.panel1);
+            this.Name = "VPrincipal";
+            this.ResumeLayout(false);
+
+        }
     }
 }
