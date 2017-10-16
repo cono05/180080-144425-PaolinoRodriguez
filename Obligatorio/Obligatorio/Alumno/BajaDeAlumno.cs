5d85fd8226a33228910690b051e6acbaf3f24635
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-10-16 11:25:35 -0300

    VentanaMenuGestionAlumno, VentanaBajaDeAlumno, VentanaAltaDeAlumno, VentanaModificarAlumno

---
+++ Obligatorio/Obligatorio/Alumno/BajaDeAlumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logica;
+
+namespace Obligatorio.Alumno
+{
+    public partial class BajaDeAlumno : UserControl
+    {
+
+        private ModuloGestionAlumno moduloAlumnos;
+        private ModuloGestionDocente moduloDocentes;
+        private ModuloGestionMaterias moduloMaterias;
+
+        public BajaDeAlumno(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria)
+        {
+            InitializeComponent();
+            moduloAlumnos  = moduloAlumno;
+            moduloDocentes = moduloDocente;
+            moduloMaterias = moduloMateria;
+        }
+    }
+}
---
+++ Obligatorio/Obligatorio/Alumno/MenuGestionAlumno.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Obligatorio
+{
+    partial class MenuGestionAlumno
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonVolver = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBaja = new System.Windows.Forms.Button();
+            this.buttonModificar = new System.Windows.Forms.Button();
+            this.buttonAlta = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonVolver);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonBaja);
+            this.panel1.Controls.Add(this.buttonModificar);
+            this.panel1.Controls.Add(this.buttonAlta);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(875, 406);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonVolver
+            // 
+            this.buttonVolver.Location = new System.Drawing.Point(388, 320);
+            this.buttonVolver.Name = "buttonVolver";
+            this.buttonVolver.Size = new System.Drawing.Size(94, 51);
+            this.buttonVolver.TabIndex = 4;
+            this.buttonVolver.Text = "Volver al Menú Principal";
+            this.buttonVolver.UseVisualStyleBackColor = true;
+            this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(326, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Menú Gestión Alumnos";
+            // 
+            // buttonBaja
+            // 
+            this.buttonBaja.Location = new System.Drawing.Point(595, 165);
+            this.buttonBaja.Name = "buttonBaja";
+            this.buttonBaja.Size = new System.Drawing.Size(94, 51);
+            this.buttonBaja.TabIndex = 2;
+            this.buttonBaja.Text = "Baja Alumno";
+            this.buttonBaja.UseVisualStyleBackColor = true;
+            this.buttonBaja.Click += new System.EventHandler(this.buttonBaja_Click);
+            // 
+            // buttonModificar
+            // 
+            this.buttonModificar.Location = new System.Drawing.Point(388, 165);
+            this.buttonModificar.Name = "buttonModificar";
+            this.buttonModificar.Size = new System.Drawing.Size(94, 51);
+            this.buttonModificar.TabIndex = 1;
+            this.buttonModificar.Text = "Modificar Alumno";
+            this.buttonModificar.UseVisualStyleBackColor = true;
+            this.buttonModificar.Click += new System.EventHandler(this.buttonModificar_Click);
+            // 
+            // buttonAlta
+            // 
+            this.buttonAlta.Location = new System.Drawing.Point(182, 165);
+            this.buttonAlta.Name = "buttonAlta";
+            this.buttonAlta.Size = new System.Drawing.Size(94, 51);
+            this.buttonAlta.TabIndex = 0;
+            this.buttonAlta.Text = "Alta Alumno";
+            this.buttonAlta.UseVisualStyleBackColor = true;
+            this.buttonAlta.Click += new System.EventHandler(this.buttonAlta_Click);
+            // 
+            // MenuGestionAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "MenuGestionAlumno";
+            this.Size = new System.Drawing.Size(875, 406);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonBaja;
+        private System.Windows.Forms.Button buttonModificar;
+        private System.Windows.Forms.Button buttonAlta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonVolver;
+    }
+}
