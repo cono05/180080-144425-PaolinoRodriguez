0f3d1cd5ade8f81a2747c0afc336794089038151
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-10-16 13:42:55 -0300

    VentanaBajaDeAlumno

--- Obligatorio/Obligatorio/VentanasDeAlumno/BajaDeAlumno.Designer.cs
+++ Obligatorio/Obligatorio/VentanasDeAlumno/BajaDeAlumno.Designer.cs
@@ -29,15 +29,62 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.listBoxAlumnos = new System.Windows.Forms.ListBox();
+            this.buttonVolver = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.listBoxAlumnos);
+            this.panel1.Controls.Add(this.buttonVolver);
+            this.panel1.Controls.Add(this.buttonEliminar);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(875, 406);
             this.panel1.TabIndex = 0;
+            // 
+            // listBoxAlumnos
+            // 
+            this.listBoxAlumnos.FormattingEnabled = true;
+            this.listBoxAlumnos.Location = new System.Drawing.Point(205, 143);
+            this.listBoxAlumnos.Name = "listBoxAlumnos";
+            this.listBoxAlumnos.Size = new System.Drawing.Size(235, 160);
+            this.listBoxAlumnos.TabIndex = 25;
+            // 
+            // buttonVolver
+            // 
+            this.buttonVolver.Location = new System.Drawing.Point(556, 252);
+            this.buttonVolver.Name = "buttonVolver";
+            this.buttonVolver.Size = new System.Drawing.Size(94, 51);
+            this.buttonVolver.TabIndex = 24;
+            this.buttonVolver.Text = "Volver";
+            this.buttonVolver.UseVisualStyleBackColor = true;
+            this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(556, 143);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(94, 51);
+            this.buttonEliminar.TabIndex = 23;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Baja De Alumno";
             // 
             // BajaDeAlumno
             // 
@@ -46,6 +93,8 @@
             this.Controls.Add(this.panel1);
             this.Name = "BajaDeAlumno";
             this.Size = new System.Drawing.Size(875, 406);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -53,5 +102,9 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBoxAlumnos;
+        private System.Windows.Forms.Button buttonVolver;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Obligatorio/Obligatorio/VentanasDeAlumno/BajaDeAlumno.cs
+++ Obligatorio/Obligatorio/VentanasDeAlumno/BajaDeAlumno.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Logica;
+using Dominio;
+using Excepciones;
 
 namespace Obligatorio.VentanasDeAlumno
 {
@@ -20,9 +22,49 @@ namespace Obligatorio.VentanasDeAlumno
         public BajaDeAlumno(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria)
         {
             InitializeComponent();
-            moduloAlumnos = moduloAlumno;
+            moduloAlumnos  = moduloAlumno;
             moduloDocentes = moduloDocente;
             moduloMaterias = moduloMateria;
+            listBoxAlumnos.DataSource = null;
+            listBoxAlumnos.DataSource = CargarListBoxAlumnos();
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            Alumno alumno = (Alumno)listBoxAlumnos.SelectedItem;
+            if (alumno != null)
+            {
+                try
+                {
+                    moduloAlumnos.Baja(alumno);
+                    listBoxAlumnos.DataSource = null;
+                    listBoxAlumnos.DataSource = CargarListBoxAlumnos();
+                }
+                catch (ExcepcionNoExisteAlumno ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex) // Ver bien que otros errores podrian suceder
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public ICollection<Alumno> CargarListBoxAlumnos()
+        {
+            ICollection<Alumno> lista = new List<Alumno>();
+            foreach (Alumno alumno in moduloAlumnos.repositorio.ObtenerAlumnos())
+            {
+                lista.Add(alumno);
+            }
+            return lista;
+        }
+
+        private void buttonVolver_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel1.Controls.Add(new MenuGestionAlumno(ref moduloAlumnos, ref moduloDocentes, ref moduloMaterias));
         }
     }
 }
