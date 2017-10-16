e33ef520b4966a91ee8156ee49fa277a5681dcbc
cono_144425 <rodriguezcono7@gmail.com>
2017-10-16 19:15:19 -0300

    [REFACTOR] Clase Camioneta

---
+++ Obligatorio/Pruebas/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class ModuloGestionCamionetasTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
