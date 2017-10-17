8c9cd86b24dc4951ec0356bb0e99b412b6532661
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-10-17 13:28:32 -0300

    Se arreglan errores de compilacion luego de merge mal realizado

--- Obligatorio/Obligatorio/VentanasDeAlumno/ModificarAlumno.cs
+++ Obligatorio/Obligatorio/VentanasDeAlumno/ModificarAlumno.cs
@@ -18,8 +18,9 @@ namespace Obligatorio.VentanasDeAlumno
         private ModuloGestionAlumno moduloAlumnos;
         private ModuloGestionDocente moduloDocentes;
         private ModuloGestionMaterias moduloMaterias;
+        private ModuloGestionCamioneta moduloCamionetas;
 
-        public ModificarAlumno(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria)
+        public ModificarAlumno(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria, ref ModuloGestionCamioneta moduloCamioneta)
         {
             InitializeComponent();
             moduloAlumnos = moduloAlumno;
@@ -124,7 +125,7 @@ namespace Obligatorio.VentanasDeAlumno
         private void VolverBtn_Click(object sender, EventArgs e)
         {
             panel1.Controls.Clear();
-            panel1.Controls.Add(new MenuGestionAlumno(ref moduloAlumnos, ref moduloDocentes, ref moduloMaterias));
+            panel1.Controls.Add(new MenuGestionAlumno(ref moduloAlumnos, ref moduloDocentes, ref moduloMaterias, ref moduloCamionetas));
         }
     }
 }
--- Obligatorio/Obligatorio/VentanasDeDocente/BajaDeDocente.cs
+++ Obligatorio/Obligatorio/VentanasDeDocente/BajaDeDocente.cs
@@ -18,8 +18,9 @@ namespace Obligatorio.VentanasDeDocente
         private ModuloGestionAlumno moduloAlumnos;
         private ModuloGestionDocente moduloDocentes;
         private ModuloGestionMaterias moduloMaterias;
+        private ModuloGestionCamioneta moduloCamionetas;
 
-        public BajaDeDocente(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria)
+        public BajaDeDocente(ref ModuloGestionAlumno moduloAlumno, ref ModuloGestionDocente moduloDocente, ref ModuloGestionMaterias moduloMateria, ref ModuloGestionCamioneta moduloCamioneta)
         {
             InitializeComponent();
             moduloAlumnos  = moduloAlumno;
@@ -42,7 +43,7 @@ namespace Obligatorio.VentanasDeDocente
         private void VolverBtn_Click(object sender, EventArgs e)
         {
             panel1.Controls.Clear();
-            panel1.Controls.Add(new MenuGestionDocente(ref moduloAlumnos, ref moduloDocentes, ref moduloMaterias));
+            panel1.Controls.Add(new MenuGestionDocente(ref moduloAlumnos, ref moduloDocentes, ref moduloMaterias, ref moduloCamionetas));
         }
 
         private void EliminarDocenteBtn_Click(object sender, EventArgs e)
