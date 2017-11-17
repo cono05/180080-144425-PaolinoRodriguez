04ddc0b6c5a8279598e983b2e257c0bfcce78652
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-11-16 23:31:56 -0300

    Se agregan clases Contexto y RepositorioBD

---
+++ Obligatorio/Persistencia/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Persistencia
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Alumno>    Alumnos { get; set; }
+        public DbSet<Docente>   Docentes { get; set; }
+        public DbSet<Materia>   Materias { get; set; }
+        public DbSet<Camioneta> Camionetas { get; set; }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
---
+++ Obligatorio/Persistencia/RepositorioBD.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Persistencia
+{
+    public class RepositorioBD : IRepositorio
+    {
+        public void AgregarAlumno(Alumno alumno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarCamioneta(Camioneta camioneta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarDocente(Docente docente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarMateria(Materia materia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarAlumno(Alumno alumno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarCamioneta(Camioneta camioneta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarDocente(Docente docente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarMateria(Materia materia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Alumno> ObtenerAlumnos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Camioneta> ObtenerCamionetas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Docente> ObtenerDocentes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Materia> ObtenerMaterias()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
