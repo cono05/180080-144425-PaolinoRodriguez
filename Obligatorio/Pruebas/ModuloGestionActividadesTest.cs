e8d3da5495ece8143ca58acf1b4fc79cff505377
MAURIZIO\Mauricio <paolinomaurizio@gmail.com>
2017-11-20 18:55:49 -0300

    Se crea clase ModuloGestionActividadesTest

---
+++ Obligatorio/Pruebas/ModuloGestionActividadesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dominio;
+using Persistencia;
+using Logica;
+using Excepciones;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class ModuloGestionActividadesTest
+    {
+        [TestMethod]
+        public void GetNombreModuloGestionAlumnoTest()
+        {
+            RepositorioRam repositorio = UtilidadesPruebas.CrearRepositorioRamDePrueba();
+            ModuloGestionAlumno moduloAlumno = UtilidadesPruebas.CrearModuloGestionAlumnosDePrueba(ref repositorio);
+            moduloAlumno.Nombre = "moduloAlumno";
+            Assert.AreEqual("moduloAlumno", moduloAlumno.Nombre);
+        }
+    }
+}
